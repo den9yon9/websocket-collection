696992aba50550082f99d378594f66af5615612a
yong <den9yon9@gmail.com>
2019-11-18 09:56:15 +0800

    feat: 自动回收连接关闭的websocket对象

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ async function connect(options) {
     }
   } = options;
 
-  let socket = new WebSocket(url);
+  connect.client = new WebSocket(url);
 
   await new Promise((resolve, reject) => {
-    socket.addEventListener("open", function(event) {
-      console.info(`onopen: websocket连接成功`);
-      resolve("onopen: websocket连接成功");
+    connect.client.addEventListener("open", function (event) {
+      console.info(`onopen: webconnect.client连接成功`);
+      resolve("onopen: webconnect.client连接成功");
     });
 
-    socket.addEventListener("error", function(event) {
-      console.error("onerror: websocket连接失败");
-      reject(new Error("onerror: websocket连接失败"));
+    connect.client.addEventListener("error", function (event) {
+      console.error("onerror: webconnect.client连接失败");
+      reject(new Error("onerror: webconnect.client连接失败"));
     });
   });
 
-  socket.addEventListener("message", message => {
+  connect.client.addEventListener("message", message => {
     let data = JSON.parse(message.data);
     if (heartBeatEnable && data.messageType === "heart")
       return console.log(`<<< pong`);
@@ -36,27 +36,25 @@ async function connect(options) {
 
   // ping
   if (heartBeatEnable) {
-    socket.timer = setInterval(function() {
-      socket.send(ping);
+    connect.client.timer = setInterval(function () {
+      connect.client.send(ping);
       console.log(`>>> ping`);
     }, heartBeatInterval);
   }
 
-  socket.addEventListener("close", function(event) {
-    clearInterval(socket.timer);
-    console.info(`onclose: websocket已关闭    (关闭原因: ${event.reason})`);
+  connect.client.addEventListener("close", function (event) {
+    clearInterval(connect.client.timer);
+    console.info(`onclose: webconnect.client已关闭    (关闭原因: ${event.reason})`);
     if (event.code === 1000) return event.reason;
-    console.info("5s后将重新连接websocket...");
+    console.info("5s后将重新连接webconnect.client...");
     setTimeout(connect.bind(this, options), 5000);
   });
 
-  connect.client = socket;
-
-  connect.send = function(message) {
+  connect.send = function (message) {
     connect.client.send(message);
   };
 
-  connect.close = function() {
+  connect.close = function () {
     connect.client.close(1000, "主动关闭");
   };
 
